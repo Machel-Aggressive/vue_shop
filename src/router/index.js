eeb5b2687aca60298d75fcd87afed53198d59a82
276820745@qq.com <276820745@qq.com>
2022-01-06 23:30:36 +0800

    add form

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '@/components/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    redirect: ''
+  },
+  {
+    path: '/login',
+    component: Login
+  }
 ]
 
 const router = new VueRouter({
