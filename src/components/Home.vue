f2b84efacc573c53820e3fb8e47457f292698f4e
276820745@qq.com <276820745@qq.com>
2022-01-16 23:21:42 +0800

    add user

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,97 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">退出</el-button>
-    </div>
+  <el-container class="home-container">
+    <el-header>
+      <div>
+        <img src="../assets/yasuo.jpg" style="height: 60px" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          :unique-opened="true"
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          :router="true"
+          :default-active="activePath"
+        >
+          <el-submenu
+            :index="'/' + item.path"
+            v-for="item in menulist"
+            :key="item.id"
+          >
+            <template slot="title">
+              <i :class="iconObj[item.id]"></i>
+              <span>{{ item.authName }}</span>
+            </template>
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+              @click="saveNavState('/' + subItem.path)"
+            >
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{ subItem.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+          <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-    methods: {
-        logout() {
-            window.sessionStorage.removeItem('token');
-            this.$router.push('/login');
-        }
+  data() {
+    return {
+      menulist: [],
+      iconObj: {
+        125: "el-icon-s-custom",
+        103: "el-icon-message-solid",
+        101: "el-icon-s-goods",
+        102: "el-icon-s-shop",
+        145: "el-icon-s-data",
+      },
+      isCollapse: false,
+      activePath: ''
+    };
+  },
+  methods: {
+    logout() {
+      window.localStorage.removeItem("token");
+      this.$router.push("/login");
+    },
+    async getMenuList() {
+      const { data: result } = await this.$http.get("menus");
+      console.log(result);
+      if (result.meta.status != 200)
+        return this.$message.error(result.meta.msg);
+      this.menulist = result.data;
+    },
+    toggleCollapse() {
+        this.isCollapse = !this.isCollapse
+    },
+    saveNavState(activePath) {
+        window.sessionStorage.setItem('activePath', activePath)
+        this.activePath = activePath
     }
-}
+  },
+  created() {
+    this.getMenuList();
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
+};
+
 </script>
 
 <style lang="less" scoped>
